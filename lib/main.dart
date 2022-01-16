4b208a1cbf48e3fc40f23798375ff74e88206ead
thit lwin <saithitlwin@gmail.com>
2022-01-17 00:47:11 +0630

    change with moor

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,14 @@ import 'network/recipe_service.dart';
 import 'network/service_interface.dart';
 import 'ui/main_screen.dart';
 import 'data/sqlite/sqlite_repository.dart';
+import 'data/moor/moor_repository.dart';
 
 Future<void> main() async {
   _setupLogging();
   WidgetsFlutterBinding.ensureInitialized();
 
-  final repository = SqliteRepository();
+  // final repository = SqliteRepository();
+  final repository = MoorRepository();
   await repository.init();
 
   runApp(MyApp(repository: repository));
